d3e9b27525de3d02bd992b6e5e67e1f11653fa38
velut <12040076+velut@users.noreply.github.com>
2021-03-22 21:29:22 +0100

    feat: add context menu on right-click

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { app, BrowserWindow } from 'electron';
+import contextMenu from 'electron-context-menu';
+
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
+
+// Add context menu on right-click
+contextMenu();
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
