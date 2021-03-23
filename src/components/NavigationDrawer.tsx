da09befec7303c177d68f3e008d41242c61e984c
velut <12040076+velut@users.noreply.github.com>
2021-03-23 12:46:43 +0100

    feat: set max height for NavigationDrawer items

--- src/components/NavigationDrawer.tsx
+++ src/components/NavigationDrawer.tsx
@@ -8,24 +8,24 @@ import { SettingsIcon } from './SettingsIcon';
 export function NavigationDrawer() {
     return (
         <aside className="h-full border-r border-gray-300 w-14">
-            <ul className="flex flex-col items-center justify-between flex-grow h-full py-4">
-                <li className="p-2">
+            <ul className="flex flex-col items-center justify-between h-full py-4 max-h-72">
+                <li className="p-1">
                     <HomeIcon className="w-8 h-8" />
                 </li>
 
-                <li className="p-2">
+                <li className="p-1">
                     <NewIcon className="w-8 h-8" />
                 </li>
 
-                <li className="p-2">
+                <li className="p-1">
                     <LoadIcon className="w-8 h-8" />
                 </li>
 
-                <li className="p-2">
+                <li className="p-1">
                     <SettingsIcon className="w-8 h-8" />
                 </li>
 
-                <li className="p-2">
+                <li className="p-1">
                     <InfoIcon className="w-8 h-8" />
                 </li>
             </ul>
